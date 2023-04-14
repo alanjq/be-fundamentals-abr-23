2c607f2881b11fa7116e3c2180e7f8f471327cd4
Alan Jimenez <alanjq@outlook.com>
2023-04-13 21:26:16 -0500

    Archivos de la sesión

---
+++ users.js
@@ -0,0 +1,28 @@
+const userEndpoint = 'https://jsonplaceholder.typicode.com/users'
+
+fetch(userEndpoint)
+    .then((response) => response.json())
+    .then((json) => {
+        console.log('resultado', json);
+        let rowshtml = ""
+        json.map((fila) => {
+            rowshtml += createRow(fila)
+        })
+        document.getElementById('table').querySelector('tbody').innerHTML = rowshtml
+    })
+    .catch((errmsg) => {
+        console.error('Falló la API user', errmsg);
+    })
+
+// Creamos una fila para agregar a la tabla
+function createRow(user) {
+    const rowstring = `
+        <tr>
+            <td>${user.name}</td>
+            <td>${user.username}</td>
+            <td>${user.email}</td>
+            <td>${user.company.name}</td>
+        </tr>
+    `
+    return rowstring;
+}
