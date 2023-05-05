e682d527a9dd80bdee9ea744a53f1f504419624a
Alan Jimenez <alanjq@outlook.com>
2023-05-04 22:55:07 -0500

    Actividades

--- clima.js
+++ clima.js
@@ -1,14 +1,12 @@
-function findWeather() {
-    const cityName = document.querySelector("#cityname")
-
+// Obtener el clima y agregar una tarjeta
+function findWeather(city) {
     // Lista de ciudades
     const cities = document.querySelector(".ajax-section .cities")
 
     const API_KEY = "d95c58501f2b55424cb26ca37c06e37a"
 
     // API de clima
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${API_KEY}&units=metric`
-
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
 
     fetch(URL)
         .then(response => response.json())
@@ -38,7 +36,7 @@ function findWeather() {
             li.innerHTML = markup
             // Agregar el li a la lista
             cities.appendChild(li)
-            
+
             // Borrar el input y resetear el formulario
             showMessage('')
             document.querySelector('form').reset()
@@ -48,7 +46,55 @@ function findWeather() {
         .catch((error) => showMessage("Escribe una ciudad válida.", error))
 }
 
+// Lo llama el botón
+function addCityWeather() {
+    const cityName = document.querySelector("#cityname")
+    findWeather(cityName.value)
+}
+
 function showMessage(message) {
     const msgContainer = document.querySelector("#msg")
     msgContainer.textContent = message
 }
+
+// Obtiene la ubicación mediante una API
+function getGeolocationByIp() {
+    fetch("https://api.ipbase.com/v1/json/")
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.message === 'API rate limit exceeded') {
+                const mockData = {
+                    "ip": "2806:2f0:8000:c916:132:51a2:84e6:86e0",
+                    "country_code": "MX",
+                    "country_name": "Mexico",
+                    "region_code": "MX-DIF",
+                    "region_name": "Ciudad de México",
+                    "city": "",
+                    "zip_code": "14000",
+                    "time_zone": "America\/Mexico_City",
+                    "latitude": 19.288429260253906,
+                    "longitude": -99.16667175292969,
+                    "metro_code": 0
+                }
+                return mockData
+            }
+            return data
+        })
+        .then((data) => {
+            const ciudad = data.region_name
+            // Mostrar la ubicación predeterminada
+            findWeather(ciudad)
+            return data
+        })
+        .then((data) => {
+            getLocationFromIP(data.ip)
+            getTime(data.time_zone)
+            .then(time=>{
+                document.querySelector('#time').textContent = time
+            })
+        })
+        .catch()
+}
+
+// Obtener la ubicación aproximada mediante la API
+getGeolocationByIp()
